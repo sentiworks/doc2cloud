ff79220b06660a696d8e96b2deda4b6524f8012b
sentiworks <yibin.xia@gmail.com>
2019-06-29 22:19:13 -0700

    debug

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,28 +13,34 @@ class Home extends Component {
   checkAuthentication = async () => {
     const authenticated = await this.props.auth.isAuthenticated();
     if (authenticated !== this.state.authenticated) {
+      console.log("home authenticated: ", authenticated);
       this.setState({authenticated});
     }
   }
 
   async componentDidMount() {
+    console.log('home mount');
     this.checkAuthentication();
   }
 
   async componentDidUpdate() {
+    console.log("home update");
     this.checkAuthentication();
   }
 
   login = async () => {
+    console.log("home async login");
     this.props.auth.login('/');
   }
 
   logout = async () => {
+    console.log("home async logout");
     this.props.auth.logout('/');
   }
 
   render() {
     if (this.state.authenticated === null) {
+      console.log("home rendering under no authentication");
       return null;
     }
     const content = this.state.authenticated
--- src/components/Login.js
+++ src/components/Login.js
@@ -23,25 +23,44 @@ class Login extends Component {
   }
 
   componentDidUpdate() {
+    console.log("login updated");
     this.checkAuthentication();
   }
 
   handleSuccess = (res) => {
     if (res.status === 'SUCCESS') {
-      return this.props.auth.redirect({
+      console.log("user: ", res.user);
+      if (res.type === 'SESSION_STEP_UP') {
+        res.stepUp.finish();
+      } 
+      console.log("claims: ", res.claims);
+      console.log("login success: ", res.session.token);
+  //    res.session.setCookieAndRedirect(process.env.REACT_APP_OKTA_REDIRECTURI);
+      
+      this.props.auth.redirect({
         sessionToken: res.session.token
       });
+      
+      return;
     } else {
+      console.log("login unsuccess: ", this)
       // other states
     }
   }
 
   handleError = (err) => {
-    console.log("Error: ", err);
+    console.log("login Error: ", err);
+    var errorEl = document.createElement('div');
+    errorEl.textContent = 'Error! ' + err.message;
+    document.body.insertBefore(
+      errorEl,
+      document.body.firstChild
+    );
   }
 
   render() {
     if (this.state.authenticated === null) {
+      console.log("login rendering under no authentication");
       return null;
     }
     return (
